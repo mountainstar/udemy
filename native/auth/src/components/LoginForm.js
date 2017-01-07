15b04bfd1f4e65f62c1858354c9256c1dcd67ce2
Chris Wirth <chriswirth@Chriss-MacBook-Pro.local>
2017-01-06 22:03:12 -0700

    Inputs

--- native/auth/src/components/LoginForm.js
+++ native/auth/src/components/LoginForm.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { text: ''};
+
   render(){
     return(
       <Card>
         <CardSection>
-          <TextInput style={{ height: 20, width: 100 }} />
+          <Input
+            label="Email"
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            />
         </CardSection>
         <CardSection />
         <CardSection>
